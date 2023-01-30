d8f9cd39bfa42a1607d1a226e1d62a6b888880f2
Mizab <mizabmansari@gmail.com>
2023-01-30 12:57:46 +0530

    Added Converse Raycast

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,22 @@
 
-import { effect, execute, loc, MCFunction, Objective, particle, rel, say, Selector } from 'sandstone'
-import { raycast } from './RaycastLib'
+import { BLOCKS, effect, execute, loc, MCFunction, MCFunctionInstance, Objective, particle, rel, say, Selector, SelectorClass } from 'sandstone'
+import { raycast, raycastConverse } from './RaycastLib'
 
 export const rightClickObj = Objective.create("cast.rc.obj", "minecraft.used:minecraft.carrot_on_a_stick")
 export const rightClick = rightClickObj("@s")
+let name: string = "raycast2/cast"
+let block: BLOCKS = "minecraft:oak_planks"
+let entity: SelectorClass = Selector('@e', {
+	type: "minecraft:creeper",
+	dx: 0
+})
+let update: MCFunctionInstance = MCFunction("raycast2/update", () => {
+	particle("minecraft:crit", rel(0, 0, 0), [0, 0, 0], 0, 1);
+})
+let hit: MCFunctionInstance = MCFunction("raycast2/hit", () => {
+	say("hi")
+})
+let steps: number = 2
 
 MCFunction('test', () => {
     execute.as(Selector('@a', { scores: { 'cast.rc.obj': [1, null] } })).at('@s').anchored("eyes").positioned(loc(0, 0, 1)).run(() => {
@@ -25,6 +38,7 @@ MCFunction('test', () => {
 				say("Hi")
 			}), 1
 		)
+		// raycast(name, block, null, update, hit, steps)
 	})
 }, {
 	runEachTick: true
